6eb02cf695b2f92b1d3b8707956e2fa56ffa4e25
inobelieve <514396432@qq.com>
2021-09-13 15:59:36 +0800

    improve demo

--- src/example/demo01.vue
+++ src/example/demo01.vue
@@ -1,27 +1,43 @@
 <template>
     <div class="container">
-        <div v-drag="true" class="drager">1234</div>
+        <div v-drag="{
+              handler: handleDrag,
+              limit
+             }" class="drager">1234</div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "demo01"
+        name: "demo01",
+        data () {
+            return {
+                limit: {
+                    y: [0, 870],
+                    x: [0, 1820]
+                }
+            }
+        },
+        methods: {
+            handleDrag (e) {
+                console.log(e)
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-.container {
-    width: 100vw;
-    height: 100vh;
-    background-color: #ffffff;
-    overflow: hidden;
-    position: relative;
-    user-select: none;
-    .drager {
-        width: 100px;
-        height: 100px;
-        background-color: #cccccc;
+    .container {
+        width: 100vw;
+        height: 100vh;
+        background-color: #ffffff;
+        overflow: hidden;
+        position: relative;
+        user-select: none;
+        .drager {
+            width: 100px;
+            height: 100px;
+            background-color: #cccccc;
+        }
     }
-}
 </style>
